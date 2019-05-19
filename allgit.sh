e42c145d422cc08bcf97a1877088db746115fe87
zhangqizhen <zhangqizhen315@gmail.com>
2019-05-19 16:31:42 +0800

    2019-05-19:16:31:S

--- allgit.sh
+++ allgit.sh
@@ -9,16 +9,16 @@ do
 #   pwd
 
 # 进入目标文件夹;
+    echo "======================"
     echo "现在开始对${i}提交 git 了"
     cd ${i}
     sudo git add .
     sudo git commit -m "`date '+%Y-%m-%d:%H:%M:S'`"
-    sudo git pull origin master:master
     sudo git checkout master
+    sudo git pull origin master:master
     sudo git merge dev
-    sudo git push origin master
+    sudo git push origin
     sudo git checkout dev
     sudo chown -R zhang:zhang *
-    echo "======================"
     echo "======================"
 done
