aebc9985ee8fd6e2f02921091b750c4f3c439206
zhangqizhen <zhangqizhen315@gmail.com>
2019-05-22 06:03:11 +0800

    2019-05-22:06:03:S

--- allgit.sh
+++ allgit.sh
@@ -22,31 +22,3 @@ do
     sudo chown -R zhang:zhang *
     echo "======================"
 done
-<<<<<<< HEAD
-
-
-
-for i in ${sina[@]}
-do
-# 进入目标文件夹;
-    echo "======================"
-    echo "现在开始对${i}提交 git 了"
-    cd ${i}
-    cd .git 
-    sudo chown -R zhang:zhang *
-    cd ..
-    git add .
-    git commit -m "`date '+%Y-%m-%d:%H:%M:S'`"
-    git checkout master
-    git pull origin 1:master
-    git merge dev
-    git push origin master:1
-    git checkout dev
-    sudo chown -R zhang:zhang *
-    echo "======================"
-done
-
-cp -f /home/zhang/myScript/.zshrc /home/zhang/
-source /home/zhang/.zshrc
-=======
->>>>>>> dev
