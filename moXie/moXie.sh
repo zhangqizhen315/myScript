a6ff777ab04edd1caa0e63fd0be4bc36fa6cf65f
zhangqizhen <zhangqizhen315@gmail.com>
2019-12-17 23:58:33 +0800

    2019-12-17:23:58:S

--- moXie/moXie.sh
+++ moXie/moXie.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-wd
 clear
 i=0
 word=test
+lineNumber=0
+cut -d "|" -f2 ~/memory/$(date +%y-%m-%d) | sort | uniq -c
 while read line
 do
     i=$((++i))
     if [ $i -eq 1 ]; then
         word=$line
-    elif [ $i -ge 3 ] && ! [[ "$line" =~ .*CET.* ]]; then
+    elif [ $i -ge 3 ] && ! [[ "$line" =~ .*CET[1-9].* ]] && ! [[ "$line" =~ .*TEM[1-9].* ]] && ! [[ "$line" =~ .*（.*[a-z]+.*\）.* ]] && ! [[ "$line" =~ .*\(.*[a-z]+.*\).* ]]; then
+        lineNumber=$((++lineNumber))
         echo $line
     fi
 done <<< $(wd $1)
+if [ $lineNumber -gt 0 ]; then
 read -p "请输入单词:" answer
 if [ "$answer" = "$1" ]; then
     read -p "对了"
@@ -24,9 +27,22 @@ if [ "$answer" = "$1" ]; then
         echo "$1|4" >> ~/memory/$(date -d "+7 day" +%y-%m-%d)
     elif [ $2 -eq 4 ]; then
         echo "$1|5" >> ~/memory/$(date -d "+15 day" +%y-%m-%d)
+    elif [ $2 -eq 0 ]; then
+        sed -i "3i$1|6" ~/memory/$( date +%y-%m-%d )
+    elif [ $2 -eq 6 ]; then
+        sed -i "5i$1|7" ~/memory/$( date +%y-%m-%d )
+    elif [ $2 -eq 7 ]; then
+        sed -i "10i$1|8" ~/memory/$( date +%y-%m-%d )
+    elif [ $2 -eq 8 ]; then
+        echo "$1|1" >> ~/memory/$( date +%y-%m-%d )
     fi
 else
     echo $1 
     read -p "错了"
-    echo "$1|1" >> ~/memory/$( date +%y-%m-%d )
+    until [ "$1" = "$answer" ]
+    do
+        read -p "请确认:" answer
+    done
+    sed -i "2i$1|0" ~/memory/$( date +%y-%m-%d )
+fi
 fi
