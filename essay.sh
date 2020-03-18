6914216f25dad33496c6db75036f54a25bc1fb42
zhangqizhen <zhangqizhen315@gmail.com>
2020-03-18 11:04:36 +0800

    2020-03-18:11:04:S

--- essay.sh
+++ essay.sh
@@ -7,13 +7,13 @@ path=~/myScript/essay/${today}
 
 if [ ! -d "${path}" ] ; then
     mkdir ${path}
-    ls -al ${path}
-    read -p "请输入文件名:" filename
-    touch ${path}/$filename
-    vim ${path}/$filename
-else
-    ls -al ${path}
-    read -p "请输入文件名:" filename
-    touch ${path}/$filename
-    vim ${path}/$filename
 fi 
+
+    ls -al ${path}
+    read -p "请输入文件名:" filename
+if [ "${filename}" = "" ] ; then
+    echo "yes"
+    filename=$( date +%H-%M-%S )
+fi
+    touch ${path}/$filename
+    vim ${path}/$filename
