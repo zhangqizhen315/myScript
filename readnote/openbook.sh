c30fcfafc2b856cf2d4e5f1a9342d438d8654d7f
zhangqizhen <zhangqizhen315@gmail.com>
2020-11-18 17:26:02 +0800

    2020-11-18:17:26:S

--- readnote/openbook.sh
+++ readnote/openbook.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 setcb() {
+    echo $1
     echo $1 > currentBook
 }
 
 if [[ "$1" =~ .*pdf$ ]] ; then 
     nohup evince $1 &
-    setcb
+    echo $1 > currentBook
 elif [[ "$1" =~ .*epub$|.*epub3$|.*azw$|.*azw3$|.*mobi$|.*fb2$|.*fb2.zip$|.*cbr$|.*cbz$|.*cbt$|.*cb7$|.*txt$ ]] ; then
     nohup /usr/bin/com.github.johnfactotum.Foliate $1 &
-    setcb
+    echo $1 > currentBook
 else
     echo "暂不支持这种电子书"
 fi
