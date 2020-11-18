095ad3ff8b1ccc254b82d22a9a8df80bf3cbe076
zhangqizhen <zhangqizhen315@gmail.com>
2020-11-18 17:00:14 +0800

    2020-11-18:17:00:S

--- myTest.sh
+++ myTest.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 # 用来试东西的脚本
 
-echo "hello"
-let a=$#==1
-echo $a
- 
-for line in $( cat $1 )
-do
-    echo "ile : $line"
-done
+fangfa (  ) {
+    echo "方法"
+}
+
+echo $1 | grep ".*xlsx$" 
+
+if [[ "$1" =~ .*xlsx$ ]] ; then
+    echo "对喽"
+    fangfa
+else
+    echo "不对"
+fi
+
+
+
+#以下是以前的测试
+#-------------------------
+#echo "hello"
+#let a=$#==1
+#echo $a
+# 
+#for line in $( cat $1 )
+#do
+#    echo "ile : $line"
+#done
 
