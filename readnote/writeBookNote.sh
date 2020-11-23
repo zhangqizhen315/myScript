b4756a89e0313f747f1fd92fb66b735704ee84fe
zhangqizhen <zhangqizhen315@gmail.com>
2020-11-23 17:00:17 +0800

    2020-11-23:17:00:S

--- readnote/writeBookNote.sh
+++ readnote/writeBookNote.sh
@@ -2,6 +2,14 @@
 
 currentBook=$( cat ./currentBook | awk -F "/" '{print $NF}' | awk -F "." '{print $1}' )
 
-echo $currentBook
+# echo $currentBook
+
+if [ ! -d ~/myContent/readnote/${currentBook} ]; then
+    mkdir ~/myContent/readnote/${currentBook}
+fi
+
+cd ~/myContent/readnote/${currentBook}
+
+vim
 
 
