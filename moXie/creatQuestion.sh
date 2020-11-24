c81d97c9c2e2bab114af64c1b2f3615561402030
zhangqizhen <zhangqizhen315@gmail.com>
2020-11-24 18:00:34 +0800

    2020-11-24:18:00:S

--- moXie/creatQuestion.sh
+++ moXie/creatQuestion.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-qa=~/myScript/moXie/qa
-qa_id=$( cat ${qa}/qaNumber )
+qa=~/myScript/moXie/
+qa_id=$( cat ${qa}/qa/qaNumber )
 qa_id=$(( ++qa_id ))
 
 read -p "请输入答案:" answer
 
-shiFouCunZai=$( grep "^${answer}.*" $qa/answerBook )
+shiFouCunZai=$( grep "^${answer}.*" $qa/answer/$(date +%y-%m-%d) )
 
 while [ "${shiFouCunZai}" ]
 do
     echo "该问题已存在"
     read -p "请输入答案:" answer
-    shiFouCunZai=$( grep "^${answer}.*" $qa/answerBook )
+    shiFouCunZai=$( grep "^${answer}.*" $qa/answer/$(date +%y-%m-%d) )
 done
 
 echo $qa_id > ${qa}/qaNumber
-echo "${answer}|${qa_id}" >> $qa/answerBook
+echo "${answer}|${qa_id}" >> $qa/answer/$(date +%y-%m-%d)
 
 touch ${qa}/${qa_id}
 
