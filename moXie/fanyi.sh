42502b590ef4a3b4addc94e71ae0e6743e9f2ed7
zhangqizhen <zhangqizhen315@gmail.com>
2020-11-26 10:00:01 +0800

    2020-11-26:10:00:S

--- moXie/fanyi.sh
+++ moXie/fanyi.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+
 while [ true ]
 do
-
+    clear
     read -p : word
     echo $word
     wd $word
     if [[ $( cat ~/myScript/moXie/memory/$( date +%y-%m-%d ) | grep "${word}|" ) = "" ]]; then
         echo "${word}|1" >> ~/myScript/moXie/memory/$( date +%y-%m-%d )
     fi
+    read
 done
